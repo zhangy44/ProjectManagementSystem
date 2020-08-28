f6f691c9c6132f27af4ff0fb53db41d3c1eabb7a
zhangy44 <yang3377zhang@gmail.com>
2020-08-28 15:39:57 -0400

    First commit

---
+++ src/test/java/com/yyz/pma/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyz.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
