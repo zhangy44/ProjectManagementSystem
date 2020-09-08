812a7d1bb7f22786b5fa642d62c96334104b325c
zhangy44 <yang3377zhang@gmail.com>
2020-09-08 19:58:18 -0400

    csrf token added and customized error pages

---
+++ src/main/java/com/yyz/pma/dao/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.yyz.pma.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.yyz.pma.entities.UserAccount;
+
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long>{
+
+}
--- src/main/java/com/yyz/pma/security/SecurityConfiguration.java
+++ src/main/java/com/yyz/pma/security/SecurityConfiguration.java
@@ -1,27 +1,58 @@
 package com.yyz.pma.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("myuser")
-		.password("pass")
-		.roles("USER");
-//		auth.jdbcAuthentication()
-//			.usersByUsernameQuery("select username, password, enabled "+
-//				"from user_accounts where username = ?" )
-//			.authoritiesByUsernameQuery("select username, role "+
-//				"from user_accounts where username = ?")
-//			.dataSource(dataSource)
-//			.passwordEncoder(bCryptEncoder);
+	//-----IN memory authentication-------
+//		auth.inMemoryAuthentication()
+//		.withUser("myuser")
+//		.password("pass")
+//		.roles("USER");
+		
+		
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery("select username, password, enabled "+
+				"from user_accounts where username = ?" )
+			.authoritiesByUsernameQuery("select username, role "+
+				"from user_accounts where username = ?")
+			.dataSource(dataSource)
+			.passwordEncoder(bCryptEncoder);
+		
+	}
+	//Authorization
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/projects/save").hasRole("ADMIN")
+			.antMatchers("/employees/new").hasRole("ADMIN")
+			.antMatchers("/employees/save").hasRole("ADMIN")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
+		//   .loginPage("login-page")
+		//then need a controller to control the login-page.html
+		
+		//http.csrf().disable();
 		
 	}
 }
